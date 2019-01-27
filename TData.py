340d805d95e400ec319b7aa28a445d264ca939d8
Harrison Wynn <harrisonwynn42@gmail.com>
2019-01-27 15:53:20 -0600

    Added tests for level 1. Preparing to add compatibility checks for future versions
    Signed-off-by: hwynn <Rocketferret54>

--- TData.py
+++ TData.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import datetime
 import MetadataManagerL0
 import MetadataManagerL1
---
+++ configManagement.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def currentVersion():
+    return '0.03'
+
+def getSoftwareName():
+    return 'quickTagger'
