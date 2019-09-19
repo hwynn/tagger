a4f163e59d33a050b7c1bba565bb9dd224da1547
Harrison Wynn <harrisonwynn42@gmail.com>
2019-09-19 17:14:41 -0500

    beginning file navigation librarys
    Signed-off-by: Harrison Wynn <harrisonwynn42@gmail.com>

---
+++ guiTesting/PathManager.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#this is a small library that:
+# stores filepaths
+# switches active filepath
+
+def currentPath():
+    # returns current active path
+    # I guess we only have one file path when we're navigating to next and previous files
+    pass
+
+def storedPaths():
+    #returns list of all stored paths
+    #we'll have several pathlists in case we want to do something like find a picture with certain tags
+    pass
+
+def addPath(x):
+    # this adds a new filepath to our list of stored paths
+    print(x)
+
+def removePath(x):
+    # this removes a new filepath from our list of stored paths
+    pass
+
+def switchPath(x):
+    #this switches our current active path
+    #new path does not need to be in the stored paths list
+    pass
--- guiTesting/fileNavigationManager.py
+++ guiTesting/fileNavigationManager.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
 
+# this is a small library that
+# keeps track of current file
+# assists in file navigation (next/prev)
+
+def currentFile():
+    # returns current active file
+    # In certain modes, we can only access/edit a single file at a time
+    pass
+
+def nextFile():
+    # this returns the name/location of the next file in the current path
+    pass
+
+def prevFile():
+    # this returns the name/location of the previous file in the current path
+    pass
+
+def switchFile(x):
+    #this switches our current active file
+    pass
