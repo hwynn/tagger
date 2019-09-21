f206ec0d3fc27a938e4437ac7f6d5416a5f037bd
Harrison Wynn <harrisonwynn42@gmail.com>
2019-09-21 15:24:48 -0500

    testing ability to change pictures within gui. Sadly we cannot load .tif files for some reason.

--- guiTesting/PictureView.py
+++ guiTesting/PictureView.py
@@ -3,23 +3,56 @@ from kivy.lang import Builder
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
 from kivy.uix.gridlayout import GridLayout
 from PrevNext import NextPrevBar
+import SimulateOutside
+from kivy.uix.image import Image
 import os
-
-
-Builder.load_string('''
-<ContainerBox>:
-	orientation: 'vertical'
-	Image:
-        source: '../pics/psyduck.jpg'
-        allow_stretch: True
-	NextPrevBar:
-''')
 
 class ContainerBox(BoxLayout):
     def __init__(self, **kwargs):
         super(ContainerBox, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.picture = Image(allow_stretch=True)
+
+        SimulateOutside.makeActiveFile(SimulateOutside.g_picFile)
+        print("filename:", SimulateOutside.g_picFile)
+        print("path:", SimulateOutside.g_path)
+        print("prev:", SimulateOutside.g_prevFile)
+        print("next:", SimulateOutside.g_nextFile)
+        print("exists:", os.path.isfile(SimulateOutside.g_path + SimulateOutside.g_picFile))
+        self.picture.source = SimulateOutside.g_path+SimulateOutside.g_picFile
+        #f_newArtist.children[0].children[0].bind(on_release=self.delayedClose)
+
+
+
+        Clock.schedule_once(lambda dt: self.add_widget(self.picture), timeout=0.1)
+        self.navigationButtons = NextPrevBar()
+        Clock.schedule_once(lambda dt: self.add_widget(self.navigationButtons), timeout=0.1)
+        print(self.navigationButtons.children)
+
+        self.prevButton = self.navigationButtons.children[1]
+        self.nextButton = self.navigationButtons.children[0]
+        print(self.prevButton.text)
+        print(self.nextButton.text)
+
+        #self.prevButton.bind()
+
+        Clock.schedule_once(lambda dt: self.goToPrevImage(), timeout=5)
+        Clock.schedule_once(lambda dt: self.goToPrevImage(), timeout=10)
+        Clock.schedule_once(lambda dt: self.goToPrevImage(), timeout=15)
+        Clock.schedule_once(lambda dt: self.goToPrevImage(), timeout=20)
+
+    def goToNextImage(self):
+        if SimulateOutside.getNext() != '':
+            SimulateOutside.makeActiveFile(SimulateOutside.getNext())
+            self.picture.source = SimulateOutside.g_path + SimulateOutside.g_picFile
+
+    def goToPrevImage(self):
+        if SimulateOutside.getNext() != '':
+            SimulateOutside.makeActiveFile(SimulateOutside.getPrev())
+            self.picture.source = SimulateOutside.g_path + SimulateOutside.g_picFile
 
 class Nested2App(App):
     def build(self):
--- guiTesting/SimulateOutside.py
+++ guiTesting/SimulateOutside.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import datetime
+import os
 """
 This just holds functions that simulate the behaviour of other functions
 """
@@ -150,3 +151,62 @@ def wipeSeries(p_filename):
     global g_series
     g_series = None
     return True
+
+# ------------------------------------
+# -----PATH AND FILE NAVIGATION-------
+# ------------------------------------
+
+g_path = "C:/Users/H/RealDocs/programming.things/projects/scraps/tagger/pics/"
+g_picFile = 'psyduck.jpg'
+g_nextFile = ""
+g_prevFile = ""
+
+def makeActiveFile(p_filename, p_path=''):
+    #this is to change file we're using, and possibly the path we're using as well
+    global g_path
+    global g_picFile
+    global g_nextFile
+    global g_prevFile
+    f_oldnext = g_nextFile
+    f_oldprev = g_prevFile
+    f_file = p_filename
+    f_path = p_path
+    if p_path=='':
+        f_path = g_path
+    #insead of checking to see if the file or path we used actually exists, we'll throw an exception
+    try:
+        g_nextFile = ""
+        g_prevFile = ""
+        f_dir = os.scandir(f_path)
+        x = False
+        #this quickly navigates the directory
+        for i_file in f_dir:
+            if x:
+                #this is an extra loop to capture the name of the next file after finding our target file
+                g_nextFile = i_file.name
+                break
+            else:
+                #print(i_file.name, "\t\t", i_file.path)
+                if i_file.name == f_file:
+                    x = True
+                    #operation is successful at this point, so these global values are changed here
+                    g_picFile = f_file
+                    g_path = f_path
+                else:
+                    g_prevFile = i_file.name
+        #if the file doesn't exist, x will have never changed. So we need to throw an exception
+        if x == False:
+            raise FileNotFoundError(f_file+" not found")
+    except Exception as ex:
+        print("makeActiveFile() error:", ex)
+        #if this didn't work, we return things to how they were before running the function
+        g_nextFile = f_oldnext
+        g_prevFile = f_oldprev
+        return False
+    return True
+
+def getNext():
+    return g_nextFile
+
+def getPrev():
+    return g_prevFile
